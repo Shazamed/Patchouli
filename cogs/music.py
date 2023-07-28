4bb770ec2b2beaa77fa85cb1376cf44ec384b5d6
Shazamed <guangliangyeo@gmail.com>
2023-07-28 15:39:56 +0800

    Moved music commands to use slash commands. Still needs some polishing and work.

--- cogs/music.py
+++ cogs/music.py
@@ -95,6 +95,12 @@ class MusicCog(commands.Cog, name='Music'):
             await self.ensure_voice(ctx)
         return guild_check
 
+    # async def check_guild(self, ctx):
+    #     guild_check = ctx.guild is not None
+    #     if guild_check:
+    #         await self.ensure_voice(ctx)
+    #     return guild_check
+
     async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError):
             await ctx.send(error.original)
@@ -103,9 +109,9 @@ class MusicCog(commands.Cog, name='Music'):
             # which contain a reason string, such as "Join a voicechannel" etc. You can modify the above
             # if you want to do things differently.
 
-    async def ensure_voice(self, ctx):
+    async def ensure_voice(self, interactions):
         """ This check ensures that the bot and command author are in the same voicechannel. """
-        player = self.bot.lavalink.player_manager.create(ctx.guild.id)
+        player = self.bot.lavalink.player_manager.create(interactions.guild.id)
         # Create returns a player if one exists, otherwise creates.
         # This line is important because it ensures that a player always exists for a guild.
 
@@ -114,28 +120,28 @@ class MusicCog(commands.Cog, name='Music'):
 
         # These are commands that require the bot to join a voicechannel (i.e. initiating playback).
         # Commands such as volume/skip etc don't require the bot to be in a voicechannel so don't need listing here.
-        should_connect = ctx.command.name in ('play','skip','stop')
-
-        if not ctx.author.voice or not ctx.author.voice.channel:
+        should_connect = interactions.command.name in ('play','skip','stop')
+        # should_connect = True
+        if not interactions.user.voice or not interactions.user.voice.channel:
             # Our cog_command_error handler catches this and sends it to the voicechannel.
             # Exceptions allow us to "short-circuit" command invocation via checks so the
             # execution state of the command goes no further.
             raise commands.CommandInvokeError('Join a voice channel first.')
 
-        v_client = ctx.voice_client
+        v_client = interactions.guild.voice_client
         if not v_client:
             if not should_connect:
                 raise commands.CommandInvokeError('Not connected.')
 
-            permissions = ctx.author.voice.channel.permissions_for(ctx.me)
+            permissions = interactions.user.voice.channel.permissions_for(interactions.guild.me)
 
             if not permissions.connect or not permissions.speak:  # Check user limit too?
                 raise commands.CommandInvokeError('I need the `CONNECT` and `SPEAK` permissions.')
 
-            player.store('channel', ctx.channel.id)
-            await ctx.author.voice.channel.connect(cls=LavalinkVoiceClient)
+            player.store('channel', interactions.channel.id)
+            await interactions.user.voice.channel.connect(cls=LavalinkVoiceClient)
         else:
-            if v_client.channel.id != ctx.author.voice.channel.id:
+            if v_client.channel.id != interactions.user.voice.channel.id:
                 raise commands.CommandInvokeError('You need to be in my voice channel.')
 
     async def track_hook(self, event):
@@ -144,9 +150,52 @@ class MusicCog(commands.Cog, name='Music'):
             guild = self.bot.get_guild(guild_id)
             await guild.voice_client.disconnect(force=True)
 
-    @commands.command()
-    async def play(self, ctx, *, arg=None):
+    @app_commands.command(name="play")
+    async def play(self, interactions: discord.Interaction, song: str):
+        await self.cog_before_invoke(interactions)
+        player = self.bot.lavalink.player_manager.get(interactions.guild.id)
 
+        if song.startswith("http"):
+            query = song
+            search = False
+        else:
+            query = f'ytsearch:{song}'
+            search = True
+
+        results = await player.node.get_tracks(query)
+        if not results or not results['tracks']:
+            return await interactions.response.send_message('Perhaps the archives are incomplete!')
+        if search:
+            tracks = results['tracks'][0:10]
+            embed_search = discord.Embed(title='Select song by replying with integer of the choice', colour=0xc566ed)
+            i = 0
+            for track in tracks:
+                i += 1
+                embed_search.add_field(name=f'{i}. {track["info"]["title"]}', value=f'{track["info"]["uri"]}', inline=False)
+            await interactions.response.send_message(embed=embed_search)
+
+            def check(m):
+                return m.author.id == interactions.user.id and m.content.isdecimal()
+
+            response_num = await self.bot.wait_for('message', check=check)
+
+        if results['loadType'] == 'PLAYLIST_LOADED':
+            tracks = results['tracks']
+            for track in tracks:
+                player.add(requester=interactions.user.id, track=track)
+        else:
+            if search:
+                track = results['tracks'][int(response_num.content)-1]
+            else:
+                track = results['tracks'][0]
+            print(track)
+            player.add(requester=interactions.user.id, track=track)
+
+        if not player.is_playing:
+            await player.play()
+
+    @commands.command()
+    async def play2(self, ctx, *, arg=None):
         player = self.bot.lavalink.player_manager.get(ctx.guild.id)
 
         if arg is None:
@@ -167,7 +216,8 @@ class MusicCog(commands.Cog, name='Music'):
             i = 0
             for track in tracks:
                 i += 1
-                embed_search.add_field(name=f'{i}. {track["info"]["title"]}', value=f'{track["info"]["uri"]}', inline=False)
+                embed_search.add_field(name=f'{i}. {track["info"]["title"]}', value=f'{track["info"]["uri"]}',
+                                       inline=False)
             await ctx.send(embed=embed_search)
 
             def check(m):
@@ -181,7 +231,7 @@ class MusicCog(commands.Cog, name='Music'):
                 player.add(requester=ctx.author.id, track=track)
         else:
             if search:
-                track = results['tracks'][int(response_num.content)-1]
+                track = results['tracks'][int(response_num.content) - 1]
             else:
                 track = results['tracks'][0]
             print(track)
@@ -190,19 +240,24 @@ class MusicCog(commands.Cog, name='Music'):
         if not player.is_playing:
             await player.play()
 
-    @commands.command()
-    async def stop(self, ctx):
-        player = self.bot.lavalink.player_manager.get(ctx.guild.id)
-        if player is None:
-            return await ctx.send("Use the !play command first")
-        if not player.is_connected:
-            return await ctx.send("I'm not in a voice channel!")
-        if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.send("You are not in my voice channel!")
+    @app_commands.command(name="stop")
+    async def stop(self, interactions: discord.Interaction):
+        player = self.bot.lavalink.player_manager.get(interactions.guild.id)
+        # if player is None:
+        #     return await interactions.send("Use the !play command first")
+        # if not player.is_connected:
+        #     return await interactions.send("I'm not in a voice channel!")
+        # if not ctx.author.voice or (player.is_connected and interactions.user.voice.channel.id != int(player.channel_id)):
+        #     return await ctx.send("You are not in my voice channel!")
+        await self.cog_before_invoke(interactions)
         player.queue.clear()
         await player.stop()
-        await self.connect_channel(ctx.guild.id, None)
-        await ctx.send('Disconnected')
+
+        # disconnect bot
+        guild_id = interactions.guild.id
+        guild = self.bot.get_guild(guild_id)
+        await guild.voice_client.disconnect(force=True)
+        await interactions.response.send_message('Disconnected')
 
     @app_commands.command(name="skip")
     async def skip(self, interactions: discord.Interaction):
@@ -216,29 +271,29 @@ class MusicCog(commands.Cog, name='Music'):
         await player.skip()
         await interactions.response.send_message("Skipping current song")
 
-    @commands.command()
-    async def shuffle(self, ctx):
-        player = self.bot.lavalink.player_manager.get(ctx.guild.id)
-        if player is None:
-            return await ctx.send("Use the !play command first")
-        if not player.is_connected:
-            return await ctx.send("I'm not in a voice channel!")
-        if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.send("You are not in my voice channel!")
+    @app_commands.command()
+    async def shuffle(self, interactions: discord.Interaction):
+        player = self.bot.lavalink.player_manager.get(interactions.guild.id)
+        # if player is None:
+        #     return await ctx.send("Use the !play command first")
+        # if not player.is_connected:
+        #     return await ctx.send("I'm not in a voice channel!")
+        # if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
+        #     return await ctx.send("You are not in my voice channel!")
         player.set_shuffle(not player.shuffle)
-        await ctx.send(f"Shuffling {player.shuffle}")
+        await interactions.response.send_message(f"Shuffling {player.shuffle}")
 
-    @commands.command(alias=["repeat"])
-    async def loop(self, ctx):
-        player = self.bot.lavalink.player_manager.get(ctx.guild.id)
-        if player is None:
-            return await ctx.send("Use the !play command first")
-        if not player.is_connected:
-            return await ctx.send("I'm not in a voice channel!")
-        if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.send("You are not in my voice channel!")
+    @app_commands.command(name="loop")
+    async def loop(self, interactions: discord.Interaction):
+        player = self.bot.lavalink.player_manager.get(interactions.guild.id)
+        # if player is None:
+        #     return await ctx.send("Use the !play command first")
+        # if not player.is_connected:
+        #     return await ctx.send("I'm not in a voice channel!")
+        # if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
+        #     return await ctx.send("You are not in my voice channel!")
         player.set_repeat(not player.repeat)
-        await ctx.send(f"Looping {player.repeat}")
+        await interactions.response.send_message(f"Looping {player.repeat}")
 
 
 async def setup(bot):
